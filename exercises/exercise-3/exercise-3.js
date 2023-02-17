50a0b0d3830813f266ce4384a5207695c269ab5c
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-02-17 23:22:02 +0000

    exercise-3 completed

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,17 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+function getOrderReceipt(orders){
+
+  console.log("QTY".padEnd(7)+ "Item".padEnd(20) + "Total");
+   let totalOrderCost = 0;
+   orders.forEach((order)=> {
+    const {itemName, quantity,unitPrice} = order;
+    const total = quantity * unitPrice;
+    totalOrderCost += total;
+    console.log(`${quantity.toString().padEnd(7)}${itemName.padEnd(20)}${total}`);
+   });
+   console.log(`\n Total: ${totalOrderCost}`);
+}
+getOrderReceipt(order);
